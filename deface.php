7285e795067e70c26b2297a38c3a1f621c6e4d41
Anistriking <167034516+SurrealSatires@users.noreply.github.com>
2025-03-22 23:30:36 +0200

    Create deface.php

---
+++ deface.php
@@ -0,0 +1,21 @@
+<?php
+echo <<<EOD
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hacked</title>
+</head>
+<body style="background-color: rgb(0, 0, 0);">
+
+<p align="center"><font color="#00ff00" face="Bradley Hand ITC" size="6">HACKED BY</font></p>
+<p align="center"><font color="#ff0000" face="Segoe Print" size="7">AL-Kab00s & Ghost-RiderX24</font></p>
+<p align="center"><font color="#ffff00" face="Bradley Hand ITC" size="5">Dont Play With The Fire</font></p>
+<p align="center"><img alt="" src="https://th.bing.com/th/id/R.b02606892d4821da59316f2ea78577db?rik=xLZb9obODaTCIw&riu=http%3a%2f%2fwww.rankflags.com%2fwp-content%2fuploads%2f2015%2f05%2fWaving-Egypt-Flag.jpg&ehk=l%2bH7BqUKQtSrGYTN7kqRwa%2fvkQecNnrAVpahJ2Nci%2fg%3d&risl=&pid=ImgRaw&r=0" border="0" height="250" width="400"></p>
+<p align="center"><font color="#ff0000" face="Britannic" size="5">Egypt is the greatest civilization, established more than 7000 years ago, but the terrorist state of Israel stole the Palestinian lands, and killed children, Do you know that Israel was established on paper in 1948! Before Egypt installed its railways, do you know that you, and your state, are nothing, nothing, nothing, I know you will be angry when you see my message, hurt yourself if you want to, but my advise is that you have to not hurt yourself, your gonna ask me "why you said that?", because we want you sound and healthy so you can see the end of Israel, to see how your terrorist state disappears</font></p>
+<p align="center"><font color="#ff0000" face="Stencil" size="5">OUR NEW VIRUS IS GONNA CRUSH ANY GARBAGE BAG COUNTRIES</font></p>
+<p align="center">&nbsp;</p>
+
+</body>
+</html>
+EOD;
+?>
